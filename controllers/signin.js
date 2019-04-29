218f1fe652d5bcbbb6727599efe9bd8a6d4fbb0c
Caio Quirino <caio.quirino.medeiros@gmail.com>
2019-04-28 23:59:53 -0300

    changes

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,15 +1,20 @@
 const handleSignIn = (req, res, db, bcrypt) => {
-    if (isEmpty(req.body)) return res.status(400).json('invalid email or password');
-    db.select('email', 'hash').from('login')
-    .where('email', '=', req.body.email)
+  if (isEmpty(req.body))
+    return res.status(400).json("invalid email or password");
+  db.select("email", "hash")
+    .from("login")
+    .where("email", "=", req.body.email)
     .then(data => {
-        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-        if (isValid) db.select('*').from('users').where('email', '=', req.body.email)
-            .then(user => res.json(user[0]))
-            .catch(error => res.status(400).json('error getting the user data'));
-        else res.status(400).json('invalid password');  
-        })
-    .catch(() => res.status(400).json('no such email in database'));
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid)
+        db.select("*")
+          .from("users")
+          .where("email", "=", req.body.email)
+          .then(user => res.json(user[0]))
+          .catch(error => res.status(400).json("error getting the user data"));
+      else res.status(400).json("invalid password");
+    })
+    .catch(() => res.status(400).json("no such email in database"));
 };
 
-module.exports = {handleSignIn };
+module.exports = { handleSignIn };
